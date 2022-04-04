a22adb6244830860f4d2d14670fbbfa33317f804
Ivona7 <102857071+Ivona7@users.noreply.github.com>
2022-04-04 21:41:37 +0200

    Ad ui
    Add UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,54 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title: string = 'tic-tac-toe';
+
+  board: string [][] = [
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+    ['', '', '','','','','','','',''],
+  ];
+  
+  currentMove: string = 'x';
+  onePlayerPopUp: boolean = false;
 
   ngOnInit(): void {
-
+    
   }
+
+
+  chooseX(): void {
+    this.currentMove = 'x';
+    this.onePlayerPopUp = false;
+  }
+
+  
+  chooseO(): void {
+    this.currentMove = 'o';
+    this.onePlayerPopUp = false;
+  }
+
+  placeXon(i: number, j:number): void {
+    console.log('poziv funkcije');
+    this.board[i][j] = this.currentMove;
+    if(this.currentMove === 'x') {
+      this.currentMove = 'o';
+    }
+    else {
+      this.currentMove = 'x';
+    }
+  }
+
+  openOnePlayerPopUp(): void {
+    this.onePlayerPopUp = true;
+  }
+
 
 }
 
