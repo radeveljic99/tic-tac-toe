01b51ff101251e2b4d1fdaf24d2e7e81fab0bb76
Rade Veljic <radeveljic99@gmail.com>
2022-04-04 18:43:29 +0200

    Add background color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tic-tac-toe';
+export class AppComponent implements OnInit {
+  title: string = 'tic-tac-toe';
+
+  ngOnInit(): void {
+
+  }
+
 }
+
